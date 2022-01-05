274ac5834fb4a655008542cf71e05ad031050dfb
Ryan Ashiruma <ryananyangu@gmail.com>
2022-01-05 20:51:47 +0300

    added access control app

---
+++ accessctrl/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccessctrlConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accessctrl'
--- buupass/urls.py
+++ buupass/urls.py
@@ -23,7 +23,7 @@ from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_jwt_token),
     path('api-token-verify/', verify_jwt_token),
     path('api-token-refresh/', refresh_jwt_token),
