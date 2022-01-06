a416218984e00d6381590d0f6f7153a14a29e945
Ryan Ashiruma <ryananyangu@gmail.com>
2022-01-06 12:58:21 +0300

    open registration to all users

--- accessctrl/serializers.py
+++ accessctrl/serializers.py
@@ -12,7 +12,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(
         write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
@@ -24,11 +23,6 @@ class RegisterSerializer(serializers.ModelSerializer):
             'password': {'required': True},
         }
 
-    # def validate(self, attrs):
-    #     if attrs['password'] != attrs['password2']:
-    #         raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-    #     return attrs
 
     def create(self, validated_data):
         user = User.objects.create(
@@ -37,7 +31,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name']
         )
-
+        # FIXME: Create role with user username
         user.set_password(validated_data['password'])
         user.save()
 
--- accessctrl/views.py
+++ accessctrl/views.py
@@ -3,10 +3,10 @@
 
 from .models import User
 from .serializers import RegisterSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
-    # permission_classes = (AllowAny,)
+    permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
