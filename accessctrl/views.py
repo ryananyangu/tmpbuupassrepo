c445bb3661045994b845a23a3e26b557419e1b5a
Ryan Ashiruma <ryananyangu@gmail.com>
2022-01-06 12:49:08 +0300

    registration view compltet

--- accessctrl/views.py
+++ accessctrl/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+
+from .models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    # permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
--- buupass/urls.py
+++ buupass/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from accessctrl.views import RegisterView
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
@@ -27,5 +28,6 @@ urlpatterns = [
     path('api-token-auth/', obtain_jwt_token),
     path('api-token-verify/', verify_jwt_token),
     path('api-token-refresh/', refresh_jwt_token),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 
 ]
